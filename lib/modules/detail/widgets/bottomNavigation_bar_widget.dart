95ac076ff4bd3804def6b3ad993a03e25588cf0a
TR.Chakaval <tr.chakaval@TRChakavals-Mac-mini.local>
2026-06-02 02:02:38 +0700

    more on detail view + product_image

--- lib/modules/detail/widgets/bottomNavigation_bar_widget.dart
+++ lib/modules/detail/widgets/bottomNavigation_bar_widget.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: file_names
 import 'package:flutter/material.dart';
-import 'package:flutter_ecommerce_app/config/theme/app_theme.dart';
 import 'package:flutter_ecommerce_app/modules/detail/controller/detail_controller.dart';
 import 'package:get/get.dart';
 
